4edf19b2cf183ebd1eea21a80b53fc5ebfd9974c
Override <122590935+TuringProblem@users.noreply.github.com>
2025-11-05 21:59:08 -0500

    feat: more types, and i/o

--- learningocaml/simple_programs/calculator/_build/default/lib/math_logic.ml
+++ learningocaml/simple_programs/calculator/_build/default/lib/math_logic.ml
@@ -11,6 +11,11 @@ let mult (x: int) (y: int) = x * y
 let divide (x: int) (y: int) = if y == 0 then failwith "Not divisible by 0" else x / y
 
 
-
-
+let calculate (x: int) (y: int) (op: string): int =
+  match op with
+    | "add" -> add x y
+    | "subtract" -> sub x y
+    | "multiply" -> mult x y
+    | "divide" -> divide x y
+    | _ -> failwith "Please enter an operation"
 
--- learningocaml/simple_programs/calculator/bin/main.ml
+++ learningocaml/simple_programs/calculator/bin/main.ml
@@ -6,11 +6,26 @@ let rec print_hello (num: int) =
 
 
 
+
 let () = 
   let add_five (x: int) = x + 5 in
   let addition = Calculator.Math_logic.add (add_five 0) 5 in
   print_endline ("Addition is " ^ string_of_int addition);
 
-  print_hello 5
 
+
+  print_endline ("Please enter whether you'd like to add, subtract, multiply, or divide: ");
+  let operation = read_line () in
+  print_endline ("Please enter the first number: ");
+  let x_val = read_int () in
+  print_endline ("Please enter the second number: ");
+  let y_val = read_int () in
+
+  let result = Calculator.Math_logic.calculate x_val y_val operation in
+  
+  print_endline ("Result is " ^ string_of_int result);
+
+
+
+  print_hello 5
 
